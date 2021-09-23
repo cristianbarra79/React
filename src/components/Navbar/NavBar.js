6111010768c8c562eccb76728f22c4384392cda4
cristianbarra79 <cristianbarra79@gmail.com>
2021-09-23 20:28:44 -0300

    Se agrego navbar

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./NavBar.css"
+
+const Navbar = () => {
+    return (
+
+        <header>
+            <h1>e-commerce</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Contacto</a></li>
+                    <li><a href="#">Ayuda</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    )
+}
+
+export default Navbar
